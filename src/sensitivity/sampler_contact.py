5412995050b6a210b1e7cdbd6f877441c1e8c255
Kol21 <kovkol21@gmail.com>
2023-10-29 10:46:29 +0100

    Implement pipeline in sampler_contact.py for running simulations based on contact samples

--- src/sensitivity/sampler_contact.py
+++ src/sensitivity/sampler_contact.py
@@ -1,51 +1,40 @@
 import time
 
 import numpy as np
-from smt.sampling_methods import LHS
-import torch
 
 from src.sensitivity.sampler_base import SamplerBase
 
 
-class SamplerVaccinated(SamplerBase):
+class SamplerContact(SamplerBase):
     def __init__(self, sim_state: dict, sim_obj, n_samples):
         super().__init__(sim_state, sim_obj)
-        self.mode = "vacc"
+        self.mode = "contact"
         self.n_samples = n_samples
         self.sim_obj = sim_obj
         self.susc = sim_state["susc"]
         self.target_var = sim_state["target_var"]
 
         # Get number of elements in the upper triangular matrix
-        self.upper_tri_size = int((self.sim_obj.no_ag + 1) * self.sim_obj.no_ag / 2)
 
-        self.lhs_boundaries = {"lower": np.zeros(self.upper_tri_size),
-                               "upper": np.ones(self.upper_tri_size)
+        self.lhs_boundaries = {
+            "lower": np.zeros(self.sim_obj.upper_tri_size),
+            "upper": np.ones(self.sim_obj.upper_tri_size)
                                }
         self.batch_size = 1000
 
     def run_sampling(self):
-        n_samples = self.n_samples
-        batch_size = self.batch_size
-        # Create samples
-        bounds = np.array([bounds for bounds in self.lhs_boundaries.values()]).T
-        sampling = LHS(xlimits=bounds)
-        lhs_table = sampling(n_samples)
-        # Make sure that total vaccines given to an age group
-        # doesn't exceed the population of that age group
-        print(f"\n Simulation for {n_samples} samples ({self._get_variable_parameters()})")
-        print(f"Batch size: {batch_size}\n")
+        lhs_table = self._get_lhs_table()
+        contact_sim = list(map(self._get_sim_output_cm_entries_lockdown, lhs_table))
 
         # Calculate values of target variable for each sample
-        results = self.sim_obj.model.get_batched_output(lhs_table,
-                                                        batch_size,
-                                                        self.target_var)
+        sim_output = self.sim_obj.model.get_batched_output(contact_sim,
+                                                           self.batch_size,
+                                                           self.target_var)
         # Sort tables by target values
-        sorted_idx = results.argsort()
-        results = results[sorted_idx]
+        sorted_idx = sim_output.argsort()
+        sim_output = sim_output[sorted_idx]
         lhs_table = lhs_table[sorted_idx]
 
-        sim_output = results
         time.sleep(0.3)
 
         # Save samples, target values, and the most optimal vaccination strategy found with sampling
@@ -60,13 +49,24 @@ class SamplerVaccinated(SamplerBase):
         cm_sim = (1 - ratio_matrix) * (self.sim_obj.contact_matrix - self.sim_obj.contact_home)
         cm_sim += self.sim_obj.contact_home
         # Get output
-        output = self._get_output(cm_sim=cm_sim)
+        output = self._get_betas_from_contacts(cm_sim)
         cm_total_sim = (cm_sim * self.sim_obj.age_vector)[self.sim_obj.upper_tri_indexes]
         output = np.append(cm_total_sim, output)
         return list(output)
 
     def _get_variable_parameters(self):
         return f'{self.susc}-{self.base_r0}-{self.target_var}'
+
+    def _get_betas_from_contacts(self, cm_sim):
+        # Get output
+        cm = get_contact_matrix_from_upper_triu(rvector=cm_sim,
+                                                age_vector=self.sim_obj.age_vector.reshape(-1, ))
+        beta_lhs = self.base_r0 / self.r0generator.get_eig_val(contact_mtx=cm,
+                                                               susceptibles=self.sim_obj.susceptibles.reshape(1, -1),
+                                                               population=self.sim_obj.population)[0]
+        output = np.array([0, beta_lhs])
+        output = np.append(output, np.zeros(self.sim_obj.no_ag))
+        return output
 
 
 def get_rectangular_matrix_from_upper_triu(rvector, matrix_size):
