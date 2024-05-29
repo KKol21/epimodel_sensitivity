302641b626e88b194f66f2462df22f233670368b
Kol21 <kovkol21@gmail.com>
2024-05-29 14:58:07 +0200

    Implement optimized solution based target calculator

--- emsa/sensitivity/target_calc/output_generator.py
+++ emsa/sensitivity/target_calc/output_generator.py
@@ -1,8 +1,5 @@
-import numpy as np
 import torch
 
-from emsa.sensitivity.target_calc.sol_based.final_size_calc import FinalSizeCalculator
-from emsa.sensitivity.target_calc.sol_based.peak_calc import PeakCalculator
 from emsa.sensitivity.target_calc.r0calculator import R0Calculator
 
 
@@ -12,22 +9,20 @@ class OutputGenerator:
         self.sim_object = sim_object
         self.variable_params = variable_params
 
-    def get_output(self, lhs_table: np.ndarray) -> dict:
+    def get_output(self, lhs_table):
+        from emsa.sensitivity.target_calc.sol_based_target_calc import TargetCalc
         lhs = torch.from_numpy(lhs_table).float().to(self.sim_object.device)
-        results = {}
-        for target in self.sim_object.target_vars:
-            if target == "r0":
-                r0calc = R0Calculator(self.sim_object)
-                results[target] = r0calc.get_output(lhs_table=lhs)
-            else:
-                results[target] = self.calculate_target(lhs_table=lhs, target_var=target)
-        return results
+        targets = self.sim_object.target_vars
+        output = {}
+        target_endings = [target[-3:] for target in targets if target != "r0"]
 
-    def calculate_target(self, lhs_table: torch.Tensor, target_var: str) -> torch.Tensor:
-        if target_var.split('_')[1] == "sup":
-            target_calculator = FinalSizeCalculator(model=self.sim_object.model)
-        else:
-            target_calculator = PeakCalculator(model=self.sim_object.model)
-        return target_calculator.get_output(lhs_table=lhs_table,
-                                            batch_size=self.batch_size,
-                                            target_var=target_var)
+        if "max" in target_endings or "sup" in target_endings:
+            target_calc = TargetCalc(model=self.sim_object.model, targets=targets)
+            sol_based_output = target_calc.get_output(lhs_table=lhs, batch_size=self.batch_size)
+            output.update(sol_based_output)
+
+        if "r0" in targets:
+            r0calc = R0Calculator(self.sim_object)
+            r0s = r0calc.get_output(lhs_table=lhs)
+            output["r0"] = r0s
+        return output
---
+++ emsa/sensitivity/target_calc/sol_based_target_calc.py
@@ -0,0 +1,149 @@
+import math
+from time import time
+
+import torch
+import logging
+
+
+class TargetCalc:
+    def __init__(self, model, targets):
+        self.model = model
+        self.max_targets = [target.split('_')[:-1][0] for target in targets if target.split('_')[-1] == "max"]
+        self.sup_targets = [target.split('_')[:-1][0] for target in targets if target.split('_')[-1] == "sup"]
+
+        self.max_targets_finished = {}
+        self.sup_finished = None
+        self.max_targets_output = {}
+        self.sup_targets_output = {}
+        self.finished = None
+
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(level=logging.INFO)
+
+    def get_output(self, lhs_table: torch.Tensor, batch_size: int) -> dict[str, torch.Tensor]:
+        device = self.model.device
+        model = self.model
+
+        n_samples = lhs_table.shape[0]
+        self.finished = torch.zeros(n_samples, dtype=torch.bool, device=self.model.device)
+
+        indices = torch.IntTensor(range(0, n_samples)).to(device)
+
+        self.max_targets_output = {comp: torch.zeros(n_samples, device=device) for comp in self.max_targets}
+        self.sup_targets_output = {comp: torch.zeros(n_samples, device=device) for comp in self.sup_targets}
+        self.max_targets_finished = {comp: torch.BoolTensor(range(0, n_samples)).to(device) for comp in self.max_targets}
+        self.sup_finished = torch.BoolTensor(range(0, n_samples)).to(device)
+
+        t_limit = [0, 300]
+        y0 = torch.stack(
+            [model.get_initial_values()] * n_samples
+        ).to(device)
+        time_start = time()
+        # Iterate until all the eqs are solved or we reach t=5000
+        while indices.numel() and t_limit[1] < 5000:
+            t_eval = torch.stack(
+                [torch.arange(*t_limit)] * len(indices)
+            ).to(self.model.device)
+            ind_to_keep = []
+            print(f"\n Time limit: {t_limit[1]} \n"
+                  f" Samples left: {indices.numel()} \n")
+            for batch_idx in range(0, len(indices), batch_size):
+                print(f" Solving batch {int(batch_idx / batch_size) + 1} / {math.ceil(len(indices) / batch_size)}")
+                batch_slice = slice(batch_idx, batch_idx + batch_size)
+                curr_indices = indices[batch_slice]
+                batch = lhs_table[curr_indices]
+
+                # Solve for the current batch
+                self.model.generate_3D_matrices(samples=batch)
+                solutions = self.get_batch_solution(y0=y0[curr_indices], t_eval=t_eval[batch_slice], samples=batch)
+                # Save finished indices and outputs
+                self.save_finished_indices(solutions=solutions, indices=curr_indices)
+                self.save_output_for_finished(solutions=solutions, indices=curr_indices)
+
+                # Save the last values and indices of unfinished simulations
+                # to use as initial values in the next iteration
+                true_finished = self.get_true_finished()
+                last_val = solutions[:, -1, :]
+                batch_unfinished_indices = curr_indices[~true_finished[curr_indices]]
+                y0[batch_unfinished_indices] = last_val[~true_finished[curr_indices]]
+                ind_to_keep += batch_unfinished_indices
+            # Adjust time period
+            t_limit[0] = t_limit[1]
+            t_limit[1] += 50
+            # Remove indices of completed simulations
+            indices = indices[torch.isin(indices,
+                                         torch.Tensor(ind_to_keep).to(device))]
+        print("\n Elapsed time: ", time() - time_start)
+        return {
+            **{
+                f"{comp}_max": output for comp, output in self.max_targets_output.items()
+            },
+            **{
+                f"{comp}_sup": output for comp, output in self.sup_targets_output.items()
+            }
+        }
+
+    def get_batch_solution(self, y0: torch.Tensor, t_eval: torch.Tensor, samples: torch.Tensor) -> torch.Tensor:
+        sol = self.model.get_solution(y0=y0, t_eval=t_eval, lhs_table=samples).ys
+        if self.model.test:
+            # Check if population size changed
+            if any([abs(self.model.population.sum() - sol[i, -1, :].sum()) > 50 for i in range(sol.shape[0])]):
+                raise Exception("Unexpected change in population size!")
+        return sol
+
+    def save_finished_indices(self, solutions, indices) -> None:
+        last_val = solutions[:, -1, :]  # solutions.shape = (len(indices), t_limit, n_comp)
+        last_diff = solutions[:, -2, :] - last_val
+
+        for comp in self.max_targets:
+            if all(self.max_targets_finished[comp]):
+                continue
+            self.max_targets_finished[comp][indices] = self.max_stopping_condition(comp=comp, last_diff=last_diff)
+        self.sup_finished[indices] = self.sup_stopping_condition(last_val)
+
+    def max_stopping_condition(self, comp, last_diff):
+        comp_idx = self.model.idx(f'{comp}_0')
+        return last_diff[:, comp_idx].sum(axis=1) > 0
+
+    def sup_stopping_condition(self, last_val):
+        inf_sum = torch.zeros(last_val.shape[0], device=self.model.device)
+        for state, data in self.model.state_data.items():
+            if data.get("type") in ["infected"]:
+                inf_sum += self.model.aggregate_by_age(solution=last_val, comp=state)
+        finished = inf_sum < 1
+        return finished
+
+    def save_output_for_finished(self, solutions: torch.Tensor, indices) -> None:
+        for comp in self.max_targets:
+            finished = self.max_targets_finished[comp][indices]
+            if any(finished):
+                # Only update maximums if the current value is 0, not to overwrite the true maximum
+                maxes = self.max_targets_output[comp][indices[finished]]
+                self.max_targets_output[comp][indices[finished]] = \
+                    torch.where(maxes == 0,
+                                self.max_metric(solutions=solutions[finished], comp=comp),
+                                maxes)
+
+        finished = self.sup_finished[indices]
+        if any(finished):
+            for comp in self.sup_targets:
+                self.sup_targets_output[comp][indices[finished]] = \
+                    self.sup_metric(solutions=solutions[finished], comp=comp)
+
+    def max_metric(self, solutions, comp) -> torch.Tensor:
+        comp_max = torch.stack(
+            [torch.max(self.model.aggregate_by_age(solution=solutions[i, :, :],
+                                                   comp=comp))
+             for i in range(solutions.shape[0])]
+        ).to(self.model.device)
+        return comp_max
+
+    def sup_metric(self, solutions, comp) -> torch.Tensor:
+        return solutions[:, -1, self.model.idx(f"{comp}_0")].sum(axis=1)
+
+    def get_true_finished(self) -> torch.BoolTensor:
+        finished = self.finished
+        finished = finished | self.sup_finished
+        for comp in self.max_targets:
+            finished = finished & self.max_targets_finished[comp]
+        return finished
