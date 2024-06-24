9385240b49be4e004e36fd16640f5901ab91a27e
Kol21 <kovkol21@gmail.com>
2024-06-24 19:05:20 +0200

    Set up CI/CD and version bumping

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/KKol21/epimodel_sensitivity',
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests", "tests.*",)),
     include_package_data=True,
     package_data={
         "emsa_examples.contact_sensitivity": ["configs/*"],
