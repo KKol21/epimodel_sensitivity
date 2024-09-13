732d0b708f689ab2cae106acd43d976ba4f1901f
Kol21 <kovkol21@gmail.com>
2024-09-13 23:36:51 +0200

    Refactor generics

--- emsa/generics/simulation_generic.py
+++ emsa/generics/simulation_generic.py
@@ -1,22 +1,22 @@
 import os
 
-from emsa.sensitivity.generic_model import GenericModel
-from emsa.sensitivity.generic_sampler import GenericSampler
+from emsa.generics.generic_model import GenericModel
+from emsa.generics.generic_sampler import GenericSampler
 from emsa.utils.dataloader import PROJECT_PATH
 from emsa.utils.simulation_base import SimulationBase
 
 
 class SimulationGeneric(SimulationBase):
-    def __init__(self, data, model_struct_path, config_path):
+    def __init__(self, data, model_struct_path, sampling_config_path):
         model_struct_path = os.path.join(
             PROJECT_PATH,
             model_struct_path
         )
-        config_path = os.path.join(
+        sampling_config_path = os.path.join(
             PROJECT_PATH,
-            config_path
+            sampling_config_path
         )
-        super().__init__(data=data, model_struct_path=model_struct_path, config_path=config_path)
+        super().__init__(data=data, model_struct_path=model_struct_path, sampling_config_path=sampling_config_path)
 
         # Initalize model
         self.model = GenericModel(sim_object=self)
