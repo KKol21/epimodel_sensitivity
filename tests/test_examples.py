c607657e1ecd102ab1194d2369c47310d0c77cc0
Kol21 <kovkol21@gmail.com>
2024-09-13 23:54:16 +0200

    Add test case for SIR use case

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -3,6 +3,7 @@ import pytest
 
 from emsa_examples.SEIHR_2_age_groups import seihr_2_ag_main
 from emsa_examples.SEIR_no_age_groups import seir_no_ag_main
+from emsa_examples.SIR_no_age_groups import SIR_no_ag_main
 from emsa_examples.contact_sensitivity import contact_main
 from emsa_examples.vaccinated_sensitivity import vaccinated_main
 
@@ -13,6 +14,10 @@ def setup():
     plt.ioff()
     # Use Agg backend (does not require a display)
     plt.switch_backend("Agg")
+
+
+def test_sir_no_ag_main():
+    SIR_no_ag_main.main()
 
 
 def test_seir_no_ag_main():
