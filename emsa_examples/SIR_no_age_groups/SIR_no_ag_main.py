2fc93d85b3686acc333975b8185c1ad544723bd2
Kol21 <kovkol21@gmail.com>
2024-09-13 23:37:07 +0200

    Implement SIR example using generics

---
+++ emsa_examples/SIR_no_age_groups/SIR_no_ag_main.py
@@ -0,0 +1,40 @@
+import os
+from types import SimpleNamespace
+
+import torch
+
+from emsa.generics.simulation_generic import SimulationGeneric
+from emsa.utils.dataloader import PROJECT_PATH
+
+
+def main():
+    contact_data = torch.tensor(1)
+    age_data = torch.tensor([10000])
+    data = SimpleNamespace(
+        **{
+            "params": {},
+            "cm": contact_data,
+            "age_data": age_data,
+            "n_age": 1,
+            "device": "cpu",
+        }
+    )
+
+    model_struct_path = os.path.join(
+        PROJECT_PATH,
+        "emsa_examples/SIR_no_age_groups/configs/model_struct.json",
+    )
+    sampling_config_path = os.path.join(
+        PROJECT_PATH,
+        "emsa_examples/SIR_no_age_groups/configs/sampling_config.json",
+    )
+
+    sim = SimulationGeneric(data=data, model_struct_path=model_struct_path, sampling_config_path=sampling_config_path)
+    sim.run_sampling()
+    sim.calculate_all_prcc()
+    sim.calculate_all_p_values()
+    sim.plot_all_prcc()
+
+
+if __name__ == "__main__":
+    main()
