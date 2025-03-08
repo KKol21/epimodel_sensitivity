c3cc96cbddaef72044d32a458406bd0dec511c60
Kol21 <kovkol21@gmail.com>
2025-03-08 21:47:27 +0100

    Refactor transform_matrix to DataloaderBase improving generalization

--- emsa/utils/dataloader.py
+++ emsa/utils/dataloader.py
@@ -16,3 +16,32 @@ class DataLoaderBase(ABC):
     @property
     def n_age(self):
         return self.age_data.size(0)
+
+    def transform_matrix(self, matrix: torch.Tensor) -> torch.Tensor:
+        """
+        Perform symmetrization of contact matrix, by averaging its elements with the elements of
+        it's transpose, then dividing each row with the size of the age group corresponding to the given row.
+
+        After this, the C[i, j] element represents the average number of interactions a member of age
+        group i has with members of age group j, while C[j, i] represents the average number of
+        interactions a member of age group j has with members of age group j in a day.
+
+        For example, in the case that the population of age group i is the double of age group j:
+
+                                     C[j, i] = 2 * C[i, j]
+
+        Args:
+            matrix (torch.Tensor): The contact matrix.
+
+        Returns:
+            torch.Tensor: The transformed contact matrix.
+        """
+        # Age vector as a column vector
+        age_distribution = self.age_data.reshape((-1, 1))
+        # Matrix containing total number of contacts
+        total_contact = matrix * age_distribution
+        # Symmetrize matrix
+        output = (total_contact + total_contact.T) / 2
+        # Divide by age group sizes
+        output /= age_distribution
+        return output
--- emsa_examples/utils/dataloader_16_ag.py
+++ emsa_examples/utils/dataloader_16_ag.py
@@ -63,38 +63,5 @@ class DataLoader(DataLoaderBase):
             wb.unload_sheet(0)
             datalist = self.transform_matrix(datalist)
             contact_matrices.update({cm_type: datalist})
-        self.cm = (
-            contact_matrices["home"]
-            + contact_matrices["work"]
-            + contact_matrices["school"]
-            + contact_matrices["other"]
-        )
-
-    def transform_matrix(self, matrix: torch.Tensor) -> torch.Tensor:
-        """
-        Perform symmetrization of contact matrix, by averaging its elements with the elements of
-        it's transpose, then dividing each row with the size of the age group corresponding to the given row.
-
-        After this, the C[i, j] element represents the average number of interactions a member of age
-        group i has with members of age group j, while C[j, i] represents the average number of
-        interactions a member of age group j has with members of age group j in a day.
-
-        For example, in the case that the population of age group i is the double of age group j:
-
-                                     C[j, i] = 2 * C[i, j]
-
-        Args:
-            matrix (torch.Tensor): The contact matrix.
-
-        Returns:
-            torch.Tensor: The transformed contact matrix.
-        """
-        # Age vector as a column vector
-        age_distribution = self.age_data.reshape((-1, 1))
-        # Matrix containing total number of contacts
-        total_contact = matrix * age_distribution
-        # Symmetrize matrix
-        output = (total_contact + total_contact.T) / 2
-        # Divide by age group sizes
-        output /= age_distribution
-        return output
+        self.cm = sum([cm for cm in contact_matrices.values()])
+        self.contact_matrices = contact_matrices
